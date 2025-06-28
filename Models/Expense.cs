f1fd223f8bccb1235067c91b8d1fb709d13ecd2f
John Vincent A. Modelo <jayvee@Jayvees-MacBook-Air.local>
2025-06-28 10:30:33 +0800

    Initial commit

---
+++ Models/Expense.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AppDevActivity2.Models
+{
+    public class Expense
+    {
+        public int id { get; set; }
+        public decimal value { get; set; }
+        [Required]
+        public string? description { get; set; }
+
+    }
+}
---
+++ Models/ExpensesDBContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppDevActivity2.Models
+{
+    public class ExpensesDbContext : DbContext
+    {
+        public DbSet<Expense> Expenses { get; set; }
+
+        public ExpensesDbContext(DbContextOptions<ExpensesDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
